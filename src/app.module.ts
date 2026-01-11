d978c0ad6ec256581ccf071edac9c009c3cf4937
moto4321 <moto432195@gmail.com>
2026-01-11 20:30:24 +0900

    feat: api fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { K8sService } from './kubernetes/k8s.service';
+import { K8sController } from './kubernetes/k8s.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, K8sController],
   providers: [K8sService],
 })
 export class AppModule {}
--- src/kubernetes/k8s.controller.ts
+++ src/kubernetes/k8s.controller.ts
@@ -1,11 +1,11 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { K8sService } from './k8s.service';
 
-@Controller()
+@Controller('k8s')
 export class K8sController {
   constructor(private readonly k8sService: K8sService) {}
-  @Post()
+  @Post('namespace')
   async createNamespace(@Body('namespace') namespace: string) {
-    return await this.k8sService.createNamespace(namespace);
+    return this.k8sService.createNamespace(namespace);
   }
 }
